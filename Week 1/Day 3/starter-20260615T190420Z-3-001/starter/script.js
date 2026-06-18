7e3a22d12f4e76ad21fc36dbbfa7961f0e7389c7
Manjosh Lilhare <163665007+Manjosh-Lilhare@users.noreply.github.com>
2026-06-18 21:41:33 +0530

    problem 1 coded

--- Week 1/Day 3/starter-20260615T190420Z-3-001/starter/script.js
+++ Week 1/Day 3/starter-20260615T190420Z-3-001/starter/script.js
@@ -1,5 +1,31 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
-const calcAge = birthYear => 2037 - birthYear;
+// const x = 23;
+// const calcAge = birthYear => 2037 - birthYear;
+
+// problem 1
+const temp  = [3, -2, -6, -1, 'error', 9, 13, 15, 14, 9, 5];
+// 1.]Understanding the problem
+// 2.]Breaking up into sub-problems
+const calcTeamperatureAmplitude=function(temps){
+    let max=temp[0];
+    for (let i=0;i<=temp.length;i++){
+        if(typeof temp[i]=='number'){
+            if(temp[i]>max){
+                max=temp[i];
+            }
+            else if(temp[i]<max){
+                continue;
+            }
+            else{
+                continue;
+            }
+        }
+        else{
+            continue;
+        }
+    }
+    return max;
+}
+console.log(calcTeamperatureAmplitude(temp));
